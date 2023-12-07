0f97bcc4cc13a89fed9485e5659bea8079d6efa7
MahbubHossainFaisal <mahbubhossain249@gmail.com>
2023-12-08 01:49:01 +0600

    Updates

---
+++ precache-manifest.213e6ff9014c1673ecad154729dbcdb0.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e53a10893556cabfe8b0daddba5ec7d",
+    "url": "/portfolio-application/index.html"
+  },
+  {
+    "revision": "fd06fd7499a77fbc3547",
+    "url": "/portfolio-application/static/css/2.2189b2f8.chunk.css"
+  },
+  {
+    "revision": "3f1aa86ee052e5a0e639",
+    "url": "/portfolio-application/static/css/main.9e1d900f.chunk.css"
+  },
+  {
+    "revision": "fd06fd7499a77fbc3547",
+    "url": "/portfolio-application/static/js/2.432f023a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/portfolio-application/static/js/2.432f023a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f1aa86ee052e5a0e639",
+    "url": "/portfolio-application/static/js/main.b16e2bf9.chunk.js"
+  },
+  {
+    "revision": "7dc1cc9caab676dee021",
+    "url": "/portfolio-application/static/js/runtime-main.73f27d7a.js"
+  },
+  {
+    "revision": "ac70cced503232cfdfab469708807127",
+    "url": "/portfolio-application/static/media/AIUB.ac70cced.jpg"
+  },
+  {
+    "revision": "b5c3bacfdb4b3eaef920e13bae9dcc16",
+    "url": "/portfolio-application/static/media/College.b5c3bacf.png"
+  },
+  {
+    "revision": "f1bf9caed374a888fad914d75c0f6455",
+    "url": "/portfolio-application/static/media/Confusion.f1bf9cae.jpg"
+  },
+  {
+    "revision": "c6a0ca40bf9261036ef441f9139e8d01",
+    "url": "/portfolio-application/static/media/FoodOrder.c6a0ca40.jpg"
+  },
+  {
+    "revision": "4f1d695adf5432164eb511687906e589",
+    "url": "/portfolio-application/static/media/GuessMyNumber.4f1d695a.jpg"
+  },
+  {
+    "revision": "a1eac7824d3270fc84869269bb1bd049",
+    "url": "/portfolio-application/static/media/IET.a1eac782.jpg"
+  },
+  {
+    "revision": "71958aa7b3a2d81828610d683283eac4",
+    "url": "/portfolio-application/static/media/Iqvia.71958aa7.jpg"
+  },
+  {
+    "revision": "8c5d08680f63f05ecb00d71240472950",
+    "url": "/portfolio-application/static/media/Notes.8c5d0868.png"
+  },
+  {
+    "revision": "7d86c02051c074c8b1190e5e6a601115",
+    "url": "/portfolio-application/static/media/P5JS.7d86c020.png"
+  },
+  {
+    "revision": "cec0fb91136b662a440bfbab52b4ec51",
+    "url": "/portfolio-application/static/media/Programmer.cec0fb91.gif"
+  },
+  {
+    "revision": "6dfbe73e9436094f9c69b568a10c036e",
+    "url": "/portfolio-application/static/media/Weather.6dfbe73e.png"
+  },
+  {
+    "revision": "106d96a7736fbb88eb15cf02cf8034b4",
+    "url": "/portfolio-application/static/media/bank.106d96a7.jpg"
+  },
+  {
+    "revision": "1ab691bca72020f4641a19a4d2d6d697",
+    "url": "/portfolio-application/static/media/dsa.1ab691bc.jpg"
+  },
+  {
+    "revision": "3acf207a7e467402f7ed122686a8b84b",
+    "url": "/portfolio-application/static/media/ecommerce.3acf207a.png"
+  },
+  {
+    "revision": "ca20b2e01869e19dbef9de99f5467016",
+    "url": "/portfolio-application/static/media/expenseTracker.ca20b2e0.jpg"
+  },
+  {
+    "revision": "aae9ae9c1a050f5cd457778bd0a47064",
+    "url": "/portfolio-application/static/media/jail.aae9ae9c.jpg"
+  },
+  {
+    "revision": "92a0f0e94215d1c8541f2a7e1dcee22b",
+    "url": "/portfolio-application/static/media/tictactoe.92a0f0e9.png"
+  }
+]);
