43b145ea77a17e4af83c5a072cdf001f022ccfb5
MahbubHossainFaisal <mahbubhossain249@gmail.com>
2023-12-08 01:17:41 +0600

    updated

---
+++ build/precache-manifest.fae6d7e16c29357d6dc3f2604d07cda0.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f3b73df084a2cd2fb228a6152d3cb69",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "fd06fd7499a77fbc3547",
+    "url": "/React-Portfolio/static/css/2.2189b2f8.chunk.css"
+  },
+  {
+    "revision": "3f1aa86ee052e5a0e639",
+    "url": "/React-Portfolio/static/css/main.9e1d900f.chunk.css"
+  },
+  {
+    "revision": "fd06fd7499a77fbc3547",
+    "url": "/React-Portfolio/static/js/2.432f023a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/React-Portfolio/static/js/2.432f023a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f1aa86ee052e5a0e639",
+    "url": "/React-Portfolio/static/js/main.b16e2bf9.chunk.js"
+  },
+  {
+    "revision": "fcd868362fc8861b5d3a",
+    "url": "/React-Portfolio/static/js/runtime-main.403dd0c8.js"
+  },
+  {
+    "revision": "ac70cced503232cfdfab469708807127",
+    "url": "/React-Portfolio/static/media/AIUB.ac70cced.jpg"
+  },
+  {
+    "revision": "b5c3bacfdb4b3eaef920e13bae9dcc16",
+    "url": "/React-Portfolio/static/media/College.b5c3bacf.png"
+  },
+  {
+    "revision": "f1bf9caed374a888fad914d75c0f6455",
+    "url": "/React-Portfolio/static/media/Confusion.f1bf9cae.jpg"
+  },
+  {
+    "revision": "c6a0ca40bf9261036ef441f9139e8d01",
+    "url": "/React-Portfolio/static/media/FoodOrder.c6a0ca40.jpg"
+  },
+  {
+    "revision": "4f1d695adf5432164eb511687906e589",
+    "url": "/React-Portfolio/static/media/GuessMyNumber.4f1d695a.jpg"
+  },
+  {
+    "revision": "a1eac7824d3270fc84869269bb1bd049",
+    "url": "/React-Portfolio/static/media/IET.a1eac782.jpg"
+  },
+  {
+    "revision": "71958aa7b3a2d81828610d683283eac4",
+    "url": "/React-Portfolio/static/media/Iqvia.71958aa7.jpg"
+  },
+  {
+    "revision": "8c5d08680f63f05ecb00d71240472950",
+    "url": "/React-Portfolio/static/media/Notes.8c5d0868.png"
+  },
+  {
+    "revision": "7d86c02051c074c8b1190e5e6a601115",
+    "url": "/React-Portfolio/static/media/P5JS.7d86c020.png"
+  },
+  {
+    "revision": "cec0fb91136b662a440bfbab52b4ec51",
+    "url": "/React-Portfolio/static/media/Programmer.cec0fb91.gif"
+  },
+  {
+    "revision": "6dfbe73e9436094f9c69b568a10c036e",
+    "url": "/React-Portfolio/static/media/Weather.6dfbe73e.png"
+  },
+  {
+    "revision": "106d96a7736fbb88eb15cf02cf8034b4",
+    "url": "/React-Portfolio/static/media/bank.106d96a7.jpg"
+  },
+  {
+    "revision": "1ab691bca72020f4641a19a4d2d6d697",
+    "url": "/React-Portfolio/static/media/dsa.1ab691bc.jpg"
+  },
+  {
+    "revision": "3acf207a7e467402f7ed122686a8b84b",
+    "url": "/React-Portfolio/static/media/ecommerce.3acf207a.png"
+  },
+  {
+    "revision": "ca20b2e01869e19dbef9de99f5467016",
+    "url": "/React-Portfolio/static/media/expenseTracker.ca20b2e0.jpg"
+  },
+  {
+    "revision": "aae9ae9c1a050f5cd457778bd0a47064",
+    "url": "/React-Portfolio/static/media/jail.aae9ae9c.jpg"
+  },
+  {
+    "revision": "92a0f0e94215d1c8541f2a7e1dcee22b",
+    "url": "/React-Portfolio/static/media/tictactoe.92a0f0e9.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f9b4013ac7a02a1f0eb2ac3cdcb95ab1.js"
+  "/React-Portfolio/precache-manifest.fae6d7e16c29357d6dc3f2604d07cda0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-Portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
